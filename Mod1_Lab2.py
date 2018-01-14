643d2e689bbd67ce70fc8521d34c49a5e21954da
Dianna Tingg <dtingg@uw.edu>
2018-01-14 15:47:00 -0800

    Final

---
+++ Mod1_Lab2.py
@@ -0,0 +1,15 @@
+# get the user to input their name and store that name as a variable
+user_name = input("Enter your name: ")
+
+# get the user to input their favorite color
+user_fav_color = input("Enter your favorite color: ")
+
+# set my name and favorite color as variables
+my_name = "Dianna"
+my_fav_color = "Green"
+
+# print my favorite color is better than theirs
+print ("My favorite color is ")
+print (my_fav_color)
+print ("My favorite color is better than yours.")
+
