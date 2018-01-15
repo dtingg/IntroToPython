8f6c6cd4c276ef0e1e7f6d0f84d57bfb44b9afc5
Dianna Tingg <dtingg@uw.edu>
2018-01-14 17:56:06 -0800

    Final

---
+++ hw1.py
@@ -0,0 +1,13 @@
+"""This is a Python 3 program.  The user is supposed to enter their name, birthday, and favorite cake."""
+
+# Ask the user what their name is
+user_name = input("What is your name? ")
+
+# Ask the user when their birthday is
+user_birthday = input("When is your birthday? ")
+
+# Ask the user what their favorite type of cake is
+user_cake = input("What is your favorite type of cake? ")
+
+# print Hi user_name, I hope you get a user_cake cake for your birthday on user_birthday!
+print ("Hi " + user_name + ", I hope you get a " + user_cake + " cake for your birthday on " + user_birthday+ "!")
