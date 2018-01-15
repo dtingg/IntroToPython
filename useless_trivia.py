0fec2419ca2120f89b9288b3418c806f4e4d1e2f
Dianna Tingg <dtingg@uw.edu>
2018-01-15 15:20:04 -0800

    Changed concatenation of string

--- useless_trivia.py
+++ useless_trivia.py
@@ -10,12 +10,12 @@ age = int(input("How old are you? "))
 weight = int(input("Ok, last question. How many pounds do you weigh? "))
 
 # Print lowercase and uppercase versions of name
-print("\nIf poet ee cummings were to e-mail you, he'd address you as", name.lower())
-print("But if ee were mad, he'd call you", name.upper())
+print("\nIf poet ee cummings were to e-mail you, he'd address you as " + name.lower() + ".")
+print("But if ee were mad, he'd call you " + name.upper() + ".")
 
 # Printing name five times
 called = name * 5
-print("\nIf a small child were trying to get your attention",)
+print("\nIf a small child were trying to get your attention,",)
 print("your name would become:")
 print(called)
 
