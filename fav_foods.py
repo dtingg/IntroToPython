46ddc61eceed1952f39d2aaa3a0a7d48c46e2758
Dianna Tingg <dtingg@uw.edu>
2018-01-15 15:42:08 -0800

    Changed concatenation of string

---
+++ fav_foods.py
@@ -0,0 +1,11 @@
+# Favorite Foods program
+# Ask the user to enter their two favorite foods
+# Print out the name of a new food by joining the original food names together
+
+user_name = input("Hi, what is your name? ")
+
+fav_food1 = input("What is one of your favorite foods? ")
+
+fav_food2 = input("What is another one of your favorite foods? ")
+
+print("Maybe you should try a " + fav_food1 + fav_food2 + "!")
